ae5a789118839773f01ce27c6a2a6c47f8c977ae
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-01-17 15:08:06 +1030

    Add documentation

--- shm_linux.go
+++ shm_linux.go
@@ -14,6 +14,19 @@ import (
 
 const devShm = "/dev/shm/"
 
+// Taken from shm_open(3):
+// 	shm_open() creates and opens a new, or opens an existing, POSIX shared
+// 	memory object. A POSIX shared memory object is in effect a handle which
+// 	can be used by unrelated processes to mmap(2) the same region of shared
+// 	memory. The shm_unlink() function performs the converse operation,
+// 	removing an object previously created by shm_open().
+// 	
+// 	The operation of shm_open() is analogous to that of open(2). name
+// 	specifies the shared memory object to be created or opened. For
+// 	portable use, a shared memory object should be identified by a name of
+// 	the form /somename; that is, a null-terminated string of up to NAME_MAX
+// 	(i.e., 255) characters consisting of an initial slash, followed by one
+// 	or more characters, none of which are slashes.
 func Open(name string, flag int, perm os.FileMode) (*os.File, error) {
 	fileName := name
 
@@ -44,6 +57,13 @@ func Open(name string, flag int, perm os.FileMode) (*os.File, error) {
 	return os.NewFile(uintptr(fd), fileName), nil
 }
 
+// Taken from shm_unlink(3):
+// 	The  operation  of shm_unlink() is analogous to unlink(2): it removes a
+// 	shared memory object name, and, once all processes  have  unmapped  the
+// 	object, de-allocates and destroys the contents of the associated memory
+// 	region.  After a successful shm_unlink(),  attempts  to  shm_open()  an
+// 	object  with  the same name will fail (unless O_CREAT was specified, in
+// 	which case a new, distinct object is created).
 func Unlink(name string) error {
 	for len(name) != 0 && name[0] == '/' {
 		name = name[1:]
